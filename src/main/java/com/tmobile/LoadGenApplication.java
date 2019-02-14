ba2c13be642c9150d505bc53cc15d3c3b022f8ab
Vinod Sethumadhavan <Vinod.Sethumadhavan@T-Mobile.com>
2019-02-13 17:05:54 -0800

    First commit

---
+++ src/main/java/com/tmobile/LoadGenApplication.java
@@ -0,0 +1,12 @@
+package com.tmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadGenApplication.class, args);
+	}
+}
